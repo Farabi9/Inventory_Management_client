2900a8178f8233ce674272037bfd30f819dc4ff0
Farabi9 <ifatkhan633@gmail.com>
2022-05-22 21:47:57 +0600

    navber for phonepear

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home/Home';
+import Navber from './Shared/Navber';
 
 function App() {
  
   return (
     <div className="App">
-      
+    <Navber></Navber>
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+    </Routes>
     </div>
   );
 }
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB9gapDBdMUD6DWZvbtJZwQLp22g1yuGkE",
-  authDomain: "simple-firebase-authenti-a00b5.firebaseapp.com",
-  projectId: "simple-firebase-authenti-a00b5",
-  storageBucket: "simple-firebase-authenti-a00b5.appspot.com",
-  messagingSenderId: "734666742931",
-  appId: "1:734666742931:web:e534ea353b04d77bb68166"
+  apiKey: "AIzaSyAQPKoFTIb4tXjqDRx8Yg9TDAn_DMmaP7Y",
+  authDomain: "phone-parts-manufacturer.firebaseapp.com",
+  projectId: "phone-parts-manufacturer",
+  storageBucket: "phone-parts-manufacturer.appspot.com",
+  messagingSenderId: "960888732198",
+  appId: "1:960888732198:web:8ebefa5f15a7b884a104e7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app;
+const auth = getAuth(app)
+export default auth;
