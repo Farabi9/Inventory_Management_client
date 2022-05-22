ed8ff9a0f387691345c43adaa1351e03f3127da4
Farabi9 <ifatkhan633@gmail.com>
2022-05-22 22:38:09 +0600

    sign in for phonepear

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Home/Home';
 import Login from './Login/Login';
+import SignUp from './Login/Signup';
 import Footer from './Shared/Footer';
 import Navber from './Shared/Navber';
 
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/signup' element={<SignUp></SignUp>}></Route>
     </Routes>
     <Footer></Footer>
     </div>
