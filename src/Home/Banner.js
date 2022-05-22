1a62e76888c6ac8e6624ab7b34f3e1fc09f90edc
Farabi9 <ifatkhan633@gmail.com>
2022-05-22 21:55:59 +0600

    banner for phonepear

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import phone from '../Image/phone.webp'
+import { Link } from 'react-router-dom';
+import phone from '../Image/phone.jpg'
 
 const Banner = () => {
     return (
@@ -7,9 +8,12 @@ const Banner = () => {
   <div class="hero-content flex-col lg:flex-row-reverse">
     <img src={phone} class="lg:max-w-lg sm:max-w-sm rounded-lg shadow-2xl"  alt=''/>
     <div>
-      <h1 class="text-5xl font-bold">Box Office News!</h1>
-      <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button class="btn btn-primary">Get Started</button>
+      <h1 class="text-5xl text-red-500 font-bold uppercase">Phone Pear</h1>
+      <p className="py-6 uppercase font-bold">We Provide you all the best Phone parts. <br /> You can find here lens, camara, battery, display etc...etc. 
+      <br />
+      We provide fast delivery and customer satisfaction
+      </p>
+      <button class="btn btn-primary font-bold"><Link to='/products'>Find Items</Link></button>
     </div>
   </div>
 </div>
