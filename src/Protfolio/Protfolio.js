8e613bc669040dc00bb5ba51cdfdcddb03076d30
Farabi9 <ifatkhan633@gmail.com>
2022-05-26 16:11:52 +0600

    protfolio complete

--- src/Protfolio/Protfolio.js
+++ src/Protfolio/Protfolio.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Protfolio = () => {
     return (
-        <div>
+        <div className='mb-80'>
+            <div className=' mt-20 mb-20'>
+            <h2 className='text-red-500 text-3xl font-extrabold'>
+                1st: Phone Pear Website
+            </h2>
+            <h4 className='text-xl mt-3 font-bold'>
+            https://phone-parts-manufacturer.web.app/
+            </h4>
             
+            </div>
+            <div className='mb-20'>
+                <h2 className='text-red-500 text-3xl font-extrabold'>
+                    2nd: Warehouse Inventory Website
+                </h2>
+                <h4 className='text-xl mt-3 font-bold'>
+                https://warehouse-inventory-5bba7.firebaseapp.com/
+                </h4>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
